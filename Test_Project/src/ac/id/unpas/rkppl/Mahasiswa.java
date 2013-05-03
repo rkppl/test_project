26c32ca1c5af38d42cb224a9ea557f9319c2d59d
rkppl <ade.sukendar@unpas.ac.id>
2013-05-03 13:05:46 +0700

    create new project

---
+++ Test_Project/src/ac/id/unpas/rkppl/Mahasiswa.java
@@ -0,0 +1,29 @@
+package ac.id.unpas.rkppl;
+
+public class Mahasiswa {
+	
+	private String nama;
+	private String nrp;
+		
+	public String getNama() {
+		return nama;
+	}
+
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+
+	public String getNrp() {
+		return nrp;
+	}
+
+	public void setNrp(String nrp) {
+		this.nrp = nrp;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
