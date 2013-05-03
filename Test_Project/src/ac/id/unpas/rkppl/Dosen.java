d05e2c7cc45a5b201d8916bbec2628e8269d89e7
rkppl <ade.sukendar@unpas.ac.id>
2013-05-03 13:09:01 +0700

    new class dosen

---
+++ Test_Project/src/ac/id/unpas/rkppl/Dosen.java
@@ -0,0 +1,20 @@
+package ac.id.unpas.rkppl;
+
+public class Dosen {
+
+	private String nidn;
+		
+	public String getNidn() {
+		return nidn;
+	}
+
+	public void setNidn(String nidn) {
+		this.nidn = nidn;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
